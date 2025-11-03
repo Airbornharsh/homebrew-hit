01f4bca1e6544d7896be71422f268b68293562d2
Harsh Keshri <harshkeshri1234567@gmail.com>
2025-11-03 17:10:22 +0530

    Updated SHA

--- hit.rb
+++ hit.rb
@@ -8,7 +8,7 @@ class Hit < Formula
     sha256 "45a46365e8593e24127dceabf4b25a369062cc398f14e761f61512b26e618aa7"
   elsif Hardware::CPU.arm?
     url "https://github.com/Airbornharsh/hit/releases/download/v0.0.1/hit-0.0.1-darwin-arm64.tar.gz"
-    sha256 "d15c7632e28c924eed30991f2f4f816d126a4f9f2a70dfc34aecd5167353ade0"
+    sha256 "8af8482ba5f8e11e7af1134d031627355e411c007e5c2a60e89b95185b6d4cbb"
   end
 
   def install
